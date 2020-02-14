37358123007747f3f896f3b0e9f5604d78d600c7
Salvatore Tedde <microcipcip@gmail.com>
2020-02-14 02:53:05 +0000

    refactor: Configuring absolute imports because relative one sucks

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const rootDir = path.resolve(__dirname, '..')
+
 module.exports = ({ config }) => {
   // Enable typescript support
   config.module.rules.push({
@@ -16,6 +19,10 @@ module.exports = ({ config }) => {
         }
       }
     ]
+  })
+
+  config.resolve.alias = Object.assign(config.resolve.alias, {
+    '@src': path.resolve(rootDir, 'src')
   })
 
   config.node = {
